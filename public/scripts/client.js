0750dc1cc073790f18842030f07e639fa2327159
Nicholas Kovacs <nicholasderekkovacs@gmail.com>
2022-10-12 21:52:58 +0000

    error handling and display

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,17 +3,23 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+const escapeFunc = function (str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
 
 const createTweetElement = function (tweetObject) {
   const markup = `
 <section class = "tweet">
 <header class>
   <div> <img src=${tweetObject.user.avatars}"> </div>
-  <div>${tweetObject.user.name}</div>
-  <div class= "handle">${tweetObject.user.handle}</div>
+  <div>${escapeFunc(tweetObject.user.name)}</div>
+  <div class= "handle">${escapeFunc(tweetObject.user.handle)}</div>
 </header>
 <article>
-${tweetObject.content.text}
+${escapeFunc(tweetObject.content.text)}
 </article>
 <footer>
   <div>${timeago.format(new Date(tweetObject.created_at))}</div>
@@ -30,38 +36,73 @@ ${tweetObject.content.text}
 
 };
 
-const renderTweets = function(tweets) {
+const errorMarkup = function(errorReason){
+  $('#error').empty();
+  const markup = `
+  <header>
+  Error!
+</header>
+<article>
+  ${errorReason}
+</article>
+<section>
+  <i class="fa-solid fa-triangle-exclamation"></i>
+</section>
+
+`;
+return markup;
+}
+
+const renderTweets = function (tweets) {
   // loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
-  for (const tweetData of tweets){
+  $('#tweets').empty();
+  for (const tweetData of tweets) {
     let $tweet = createTweetElement(tweetData);
     $('#tweets').append($tweet);  // to add it to the page so we can make sure it's got all the right elements, classes, etc.
   }
 
-}
+};
 
 // Test / driver code (temporary). Eventually will get this from the server.
 
 
 
 $(document).ready(function () {
-  $.ajax('/tweets', { method: 'GET' })
-    .then((res) => renderTweets(res))
+  $("#error").slideUp(0);
 
-  $("form").on("submit", function(event) {
+  $.ajax('/tweets', { method: 'GET' })
+    .then((res) => renderTweets(res));
+
+  $("form").on("submit", function (event) {
     event.preventDefault();
-    console.log(this)
-    console.log( $( this ).serialize() );
-    $.ajax({
-      type : 'POST',
-      url : '/tweets',
-      data : $(this).serialize()
-    })
-    $.ajax('/tweets', { method: 'GET' })
-      .then((res) => renderTweets(res))
+    const textbox = document.querySelector('.textbox');
+    if (textbox.value === "" || textbox.value === null) {
+      let $error = errorMarkup("No/Null Tweet Entry")
+      $('#error').append($error)
+      $("#error").slideDown(1000);
+    } else if (textbox.value.length > 140) {
+      let $error = errorMarkup("Tweets cannot exceed 140 characters")
+      $('#error').append($error)
+      $("#error").slideDown(1000);
+      textbox.value = "";
+    } else {
+      $("#error").slideUp(1000);
+      $('#error').empty();
+      $.ajax({
+        type: 'POST',
+        url: '/tweets',
+        data: $(this).serialize()
+      }).then((res) => $.ajax('/tweets', { method: 'GET' }))
+        .then((res) => renderTweets(res));
+
+
+    }
+
   });
 });
+
 
 
 
